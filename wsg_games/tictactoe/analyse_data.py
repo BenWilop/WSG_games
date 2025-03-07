ac060c244b9cdcdce0a128392e24b055a5cc5032
BenWilop <ben.wilop@web.de>
2025-03-07 16:02:58 +0000

    analyse_data

---
+++ wsg_games/tictactoe/analyse_data.py
@@ -0,0 +1,25 @@
+import torch.distributions as dist
+
+from wsg_games.tictactoe.data import TicTacToeData
+
+def entropy(labels: t.Tensor) -> float:
+    """Minimal achievable CE loss"""
+    distribution = dist.Categorical(probs=labels)
+    ent = distribution.entropy()
+    return ent.mean().item()
+
+def print_data_statistics(tictactoe_data: TicTacToeData) -> None:
+    print("Shape Data:   ", tictactoe_data.games_data.shape)
+    print("Shape Random: ", tictactoe_data.random_move_labels.shape)
+    print("Shape Weak:   ", tictactoe_data.weak_goals_labels.shape)
+    print("Shape Strong: ", tictactoe_data.strong_goals_labels.shape)
+    print("--------------------------------------------------------")
+    print("Evals Random: ", evaluate_predictions(tictactoe_data.random_move_labels, tictactoe_data))
+    print("Evals Weak:   ", evaluate_predictions(tictactoe_data.weak_goals_labels, tictactoe_data))
+    print("Evals Strong: ", evaluate_predictions(tictactoe_data.strong_goals_labels, tictactoe_data))
+    print("--------------------------------------------------------")
+    print("Entropy Random: ", entropy(tictactoe_data.random_move_labels))
+    print("Entropy Weak:   ", entropy(tictactoe_data.weak_goals_labels))
+    print("Entropy Strong: ", entropy(tictactoe_data.strong_goals_labels))
+    print("--------------------------------------------------------")
+    
