fa87260588539d9436bb9ac640a325ac1b792ade
BenWilop <ben.wilop@web.de>
2025-03-09 10:20:45 +0000

    PRetrain

--- wsg_games/tictactoe/train/pretrain.py
+++ wsg_games/tictactoe/train/pretrain.py
@@ -28,7 +28,7 @@ def pretrain_models(experiment_folder:str, project_name: str, tictactoe_train_da
 
 
 def plot_loss_pretrain_models(experiment_folder, project_name, test_data):
-    minimal_loss_weak = 0.6724082827568054c
+    minimal_loss_weak = 0.6724082827568054
     minimal_loss_strong = 0.5945572853088379
 
     # Initialize dictionaries to store the data for each goal type.
